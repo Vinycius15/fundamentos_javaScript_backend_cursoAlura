a49a9e8be198bc097e3024542bbcbfa03dec09a4
Vinycius <viny.antunes@gmail.com>
2022-02-17 22:54:09 -0300

    aprendendo sobre variaveis var, let e const

---
+++ aula2/variaveis.js
@@ -0,0 +1,43 @@
+// var
+    //pode usar antes de declarar
+
+        //var altura = 5;
+        //var comprimento = 7;
+
+        //area = altura * comprimento;
+       // console.log(area);
+        //var area; // pode declarar depois
+//-----------------------------------------------
+// let --criada em 2015
+
+    //let forma = "retangulo";
+    //let altura = 5;
+    //let comprimento = 7;
+    //let area; // se não for antes da err o
+
+    //if(forma === "retangulo"){
+    //    area = altura * comprimento;  
+    //} else{
+    //    area = altura * (comprimento/2);
+    //}
+
+   //console.log(area);
+
+    //let area;
+//----------------------------------------------------
+// const
+
+    //const forma = "quadrado";
+    //const altura = 5;
+    //const comprimento = 7;
+        //const area;
+    //let area;
+
+    //if(forma === "quadrado"){
+    //    area = altura * comprimento;  
+    //} else{
+    //    area = altura * (comprimento/2);
+    //}
+
+    //const area = altura * comprimento;
+    //console.log(area)
